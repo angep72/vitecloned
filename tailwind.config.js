050203c54e0bd2d74e8c6abe742be740858dec01
angep72 <p.umunyana@alustudent.com>
2024-05-17 11:50:05 +0200

    main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*"],
-  theme: {
-    extend: {},
+    theme: {
+     
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          md: "4rem",
+          lg: "6rem",
+          xl: "8rem",
+          "2xl": "12rem",
+          "3xl": "16rem",
+          "4xl": "20rem",
+        },
+        margin: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          md: "4rem",
+          lg: "6rem",
+          xl: "8rem",
+          "2xl": "12rem",
+          "3xl": "16rem",
+          "4xl": "20rem",
+        },
+      },
+      extend: {},
   },
   plugins: [],
 }
